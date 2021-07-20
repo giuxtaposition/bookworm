16b8d75d50cf787b68c2b173fc0f4921d3fee23d
giuxtaposition <yg97.cs@gmail.com>
2021-07-20 08:22:58 +0200

    if adding book and user not signed in redirect to sign in

--- src/components/Book/Book.js
+++ src/components/Book/Book.js
@@ -1,14 +1,56 @@
-import { VStack, useColorModeValue } from '@chakra-ui/react'
+import { VStack, useColorModeValue, useToast } from '@chakra-ui/react'
 import { useRouteMatch } from 'react-router-dom'
 import { SEARCH_BOOK } from '../../graphql/queries'
 import { useQuery } from '@apollo/client'
 import BookUpperBox from './BookUpperBox'
 import BookLowerBox from './BookLowerBox'
 import LoadingSpinner from '../LoadingSpinner'
+import { useMutation } from '@apollo/client'
+import { ADD_BOOK } from '../../graphql/mutations'
+import { ALL_BOOKS } from '../../graphql/queries'
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 
-const Book = () => {
+const Book = user => {
   const upperBoxBackground = useColorModeValue('#44aca0', '#065151')
   const lowerBoxBackground = useColorModeValue('white', 'gray.800')
+  const toast = useToast()
+  const location = useLocation()
+
+  const [addBook] = useMutation(ADD_BOOK, {
+    onError: error => {
+      console.log(error)
+      toast({
+        title: 'Error',
+        description: error.graphQLErrors[0].message,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      })
+    },
+    update(cache, { data: { addBook } }) {
+      cache.modify({
+        fields: {
+          allBooks(existingBooks = []) {
+            const newBookRef = cache.writeQuery({
+              data: addBook,
+              query: ALL_BOOKS,
+            })
+            return [...existingBooks, newBookRef]
+          },
+        },
+      })
+    },
+    onCompleted: () => {
+      toast({
+        title: 'Book Added.',
+        description: 'Book added with success!',
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+      })
+    },
+  })
 
   const match = useRouteMatch('/book/:id')
 
@@ -17,6 +59,29 @@ const Book = () => {
       id: match.params.id,
     },
   })
+
+  useEffect(() => {
+    const handleAdd = async () => {
+      if (location.state) {
+        console.log(location.state)
+        const bookToAdd = location.state.bookToAdd
+        await addBook({
+          variables: {
+            readState: 'unread',
+            title: bookToAdd.title,
+            id: bookToAdd.id,
+            published: bookToAdd.published,
+            author: bookToAdd.author,
+            genres: bookToAdd.genres,
+            cover: bookToAdd.cover,
+            pages: bookToAdd.pages,
+          },
+        })
+      }
+    }
+
+    handleAdd()
+  }, [addBook, location.state])
 
   return (
     <VStack
@@ -36,6 +101,8 @@ const Book = () => {
           <BookLowerBox
             lowerBoxBackground={lowerBoxBackground}
             book={data.searchBook}
+            user={user}
+            addBook={addBook}
           />
         </VStack>
       )}
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,4 +1,10 @@
-import { VStack, Heading, SimpleGrid, useMediaQuery } from '@chakra-ui/react'
+import {
+  VStack,
+  Heading,
+  SimpleGrid,
+  useMediaQuery,
+  useToast,
+} from '@chakra-ui/react'
 import BookCardList from './BookCardList'
 import { useLocation } from 'react-router-dom'
 import { useLazyQuery } from '@apollo/client'
@@ -7,16 +13,65 @@ import SearchBar from './SearchBar'
 import EmptySearchResults from './EmptySearchResults'
 import { useEffect } from 'react'
 import SearchLoadingSkeleton from './SearchLoadingSkeleton'
+import { useMutation } from '@apollo/client'
+import { ADD_BOOK } from '../../graphql/mutations'
+import { ALL_BOOKS } from '../../graphql/queries'
 
 function useQueryParams() {
   return new URLSearchParams(useLocation().search)
 }
 
-const Search = () => {
+const Search = props => {
+  const location = useLocation()
   const [isLargerThan1280] = useMediaQuery('(min-width: 1280px)')
   const queryParams = useQueryParams()
   const search = queryParams.get('q')
   const filter = queryParams.get('filter')
+  const toast = useToast()
+
+  const [addBook] = useMutation(ADD_BOOK, {
+    onError: error => {
+      console.log(error)
+      toast({
+        title: 'Error',
+        description: error.graphQLErrors[0].message,
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      })
+    },
+    update(cache, { data: { addBook } }) {
+      cache.modify({
+        fields: {
+          allBooks(existingBooks = []) {
+            const newBookRef = cache.writeQuery({
+              data: addBook,
+              query: ALL_BOOKS,
+            })
+            return [...existingBooks, newBookRef]
+          },
+        },
+      })
+
+      cache.modify({
+        id: `searchedBook:${addBook.googleId}`,
+        fields: {
+          inLibrary() {
+            return true
+          },
+        },
+      })
+    },
+    onCompleted: () => {
+      toast({
+        title: 'Book Added.',
+        description: 'Book added with success!',
+        status: 'success',
+        duration: 9000,
+        isClosable: true,
+      })
+    },
+  })
 
   const [searchBooks, { loading, data }] = useLazyQuery(SEARCH_BOOKS, {
     variables: {
@@ -31,6 +86,28 @@ const Search = () => {
       searchBooks()
     }
   }, [search, searchBooks])
+
+  useEffect(() => {
+    const handleAdd = async () => {
+      if (location.state) {
+        const bookToAdd = location.state.bookToAdd
+        await addBook({
+          variables: {
+            readState: 'unread',
+            title: bookToAdd.title,
+            id: bookToAdd.id,
+            published: bookToAdd.published,
+            author: bookToAdd.author,
+            genres: bookToAdd.genres,
+            cover: bookToAdd.cover,
+            pages: bookToAdd.pages,
+          },
+        })
+      }
+    }
+
+    handleAdd()
+  }, [addBook, location.state])
 
   if (!search) {
     return (
@@ -62,6 +139,8 @@ const Search = () => {
               genres={result.genres}
               published={result.published}
               inLibrary={result.inLibrary}
+              user={props.user}
+              addBook={addBook}
             />
           ))}
         </SimpleGrid>
